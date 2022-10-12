493356fc1615e6d86dedf1b730d69fab7cea6e99
BharatiyaKaran <kk.engineer@gmail.com>
2022-10-12 19:20:49 +0530

    graph: add problem description clone graph

--- graph/clone_graph.py
+++ graph/clone_graph.py
@@ -1,3 +1,15 @@
+"""
+Problem Description
+Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+
+Note: The test cases are generated in the following format (use the following format to use See Expected Output option):
+First integer N is the number of nodes.
+Then, N intgers follow denoting the label (or hash) of the N nodes.
+Then, N2 integers following denoting the adjacency matrix of a graph, where Adj[i][j] = 1 denotes presence of an undirected edge between the ith and jth node, O otherwise.
+
+"""
+
+
 # Definition for a undirected graph node
 class UndirectedGraphNode:
     def __init__(self, x):
